e49a3ec2fa032934a2a382b3a0428e3dfe0b9f98
Nam Truong <36851957+jvamtruong@users.noreply.github.com>
2020-01-18 01:32:55 +0700

    Add files via upload

---
+++ Unfollow_on_Facebook.js
@@ -0,0 +1,12 @@
+// source: https://www.quora.com/How-do-I-unfollow-everyone-on-Facebook-at-once
+
+var a = document.getElementsByClassName("_5u3n");
+var x = 0;
+var time = 300;
+function amol() {
+    a[x].click();
+    if(x++ < a.length){
+        setTimeout(amol, time);
+    }
+}
+amol();
