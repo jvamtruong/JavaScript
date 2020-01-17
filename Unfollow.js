f2fa755c8bc64c7df4e5801c90733f33ad480c19
Nam Truong <36851957+jvamtruong@users.noreply.github.com>
2020-01-18 01:28:17 +0700

    Update Unfollow.js

--- Unfollow.js
+++ Unfollow.js
@@ -4,7 +4,7 @@ var a = document.getElementsByClassName("_5u3n");
 var x = 0;
 var time = 300;
 function amol() {
-	a[x].click();
+		a[x].click();
     if(x++ < a.length){
 		setTimeout(amol, time);
     }
